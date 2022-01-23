6ddc5e95f2618f7204071c49a68b4ac6972f6504
Cutekittycat <81061612+Cutekittycat@users.noreply.github.com>
2022-01-23 14:47:38 +1300

    Add files via upload

--- Picnic_Bag_List.py
+++ Picnic_Bag_List.py
@@ -3,15 +3,26 @@ import random
 
 root=Tk()
 root.title("Picnic Bag List")
-root.geometry("400x400")
+root.geometry("500x500")
 
-picnic_list_display = Label(root)
-picnic_random_number = Label(root)
+label1 = Label(root)
+label2 = Label(root)
 
-def picnic():
-    List1=['Phone','PicnicBasket','Food','Water','Cake','Mum','Dad',]
-    picnic["text"] = "listed_items : " + str(picnic)
+List1=['Phone','PicnicBasket','Food','Water','Cake','Mum','Dad','Necklace','Mat']
+label1['text'] = "Listed_items :  " + str(List1)
+
+def bag_contents():
+    
+    random_list = random.sample(range(0,8),2)
+    label2['text'] = "Put items no " + str(random_list) + "In bag"
+   
+    
+label1.place(relx=0.5,rely=0.4,anchor=CENTER)
+
+btn = Button(root, text="Which item to put in bag?",command = bag_contents,bg="cyan",fg="white")
+btn.place(relx=0.5,rely=0.5,anchor=CENTER)
 
 
+label2.place(relx=0.5,rely=0.6,anchor=CENTER)
 
 root.mainloop()
